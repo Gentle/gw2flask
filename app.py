086fb767734095e8707a43618d55f6101b92dd96
Ramon Klass <tier@schokokeks.org>
2014-02-15 19:55:25 +0100

    some code moves mainly

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+from .t6price import t6price
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        sites = [{'name': "T6 prices", 'path': "t6price"},
+             ]
+        return render_template('index.html', sites=sites)
+
+    app.register_blueprint(t6price, url_prefix='/t6price')
+
+    return app
