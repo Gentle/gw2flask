6fdb1d9be8af800f57184383294aa111c84904f6
Ramon Klass <tier@schokokeks.org>
2014-02-22 03:35:32 +0100

    move libs and fix gevent error on close

--- manage.py
+++ manage.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+from gevent import monkey
+monkey.patch_all()
+
+import os
+import sys
+
+# the path to the bundled libraries
+sys.path.insert(0,os.path.join(os.path.dirname(__file__), 'libs'))
+
 from flask.ext.script import Manager
 
 from app import create_app
@@ -8,8 +17,6 @@ manager = Manager(app)
 
 @manager.command
 def import_all():
-    from gevent import monkey
-    monkey.patch_all()
     from importer import import_all
     import_all()
 
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,8 @@
+import os, sys
+
+# the path to the bundled libraries
+sys.path.insert(0,os.path.join(os.path.dirname(__file__), 'libs'))
+
 from app import create_app
 
 application = create_app()
